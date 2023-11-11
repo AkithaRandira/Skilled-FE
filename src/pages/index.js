ca99d285bee1274660ee9b69491a614cf1568933
LaXnZ <sumudithalanz@gmail.com>
2023-11-11 20:28:49 +0530

    adding companies, everything, footer, herobanner, joinskilled, popularservices, services, skilledbusiness, skilledlogo components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import Companies from "@/components/Companies";
+import Everything from "@/components/Everything";
+import HeroBanner from "@/components/HeroBanner";
+import JoinSkilled from "@/components/JoinSkilled";
+import PopularServices from "@/components/PopularServices";
+import Services from "@/components/Services";
+import SkilledBusiness from "@/components/SkilledBusiness";
+import React from "react";
 
 function index() {
   return (
-    <div className='text-lg text-red-100'>
-      Index
+    <div>
+      <HeroBanner />
+      <Companies />
+      <PopularServices />
+      <Everything />
+      <Services />
+      <SkilledBusiness />
+      <JoinSkilled />
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
